1c9272049c31dfd628ddea7c4770959bc5f4b4a7
dtogan <fofappleid@gmail.com>
2016-12-13 16:58:02 +0200

    Changed elem position

--- Task2-Parallax/js/app.js
+++ Task2-Parallax/js/app.js
@@ -2,6 +2,6 @@ window.addEventListener('scroll', function(e) {
     console.log(document.body.scrollTop);
 
     var $parallax = document.getElementById('parallax');
-    var posY = document.body.scrollTop * 0.65;
+    var posY = document.body.scrollTop * 2;
     $parallax.style.backgroundPosition = '0px ' + (-posY) + 'px';
 });
